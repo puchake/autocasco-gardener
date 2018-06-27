24bcac0a837c9c4853d227914e125b04f7cf2eab
puchake <michal.gorecki.gda@gmail.com>
2018-06-28 00:42:59 +0200

    initial commit with test code

---
+++ code/code.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+
+
+void setup() {
+  pinMode(D0, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(D0, HIGH);
+  delay(1000);
+  digitalWrite(D0, LOW); 
+  delay(1000);
+}
+ 
