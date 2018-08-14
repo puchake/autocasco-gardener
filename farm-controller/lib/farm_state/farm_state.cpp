4a2dddc2b849789b5904ea1f2f2a6ee22dff1f0c
puchake <michal.gorecki.gda@gmail.com>
2018-08-14 12:36:49 +0200

    add farm state definitions and initial values

---
+++ farm-controller/lib/farm_state/farm_state.cpp
@@ -0,0 +1,19 @@
+#include "state.hpp"
+
+
+const FarmStatus INITIAL_FARM_STATUS = {
+  is_pump_on: false
+};
+
+const FarmConfig INITIAL_FARM_CONFIG = {
+  pump_activation_water_level: 10.0,
+  pump_deactiviation_water_level: 15.0,
+  no_water_warning_delay_ms: 5000,
+  led_strip_red_intensity: 255,
+  led_strip_green_intensity: 0,
+  led_strip_blue_intensity: 255
+};
+
+const SensorsReadings INITIAL_SENSORS_READINGS = {
+  water_level: 12.5
+};
---
+++ farm-controller/lib/farm_state/farm_state.hpp
@@ -0,0 +1,37 @@
+#ifndef __STATE_HPP_INCLUDED__
+#define __STATE_HPP_INCLUDED__
+
+
+#include <ESP8266WiFI.h>
+
+
+struct FarmStatus {
+  bool is_pump_on;
+  // TODO: add the rest of the status
+};
+
+
+struct FarmConfig {
+  float pump_activation_water_level;
+  float pump_deactiviation_water_level;
+  int no_water_warning_delay_ms;
+
+  uint8_t led_strip_red_intensity;
+  uint8_t led_strip_green_intensity;
+  uint8_t led_strip_blue_intensity;
+  // TODO: add the rest of the config
+};
+
+
+struct SensorsReadings {
+  float water_level;
+  // TODO: add the rest of the readings
+};
+
+
+extern const FarmStatus INITIAL_FARM_STATUS;
+extern const FarmConfig INITIAL_FARM_CONFIG;
+extern const SensorsReadings INITIAL_SENSORS_READINGS;
+
+
+#endif
